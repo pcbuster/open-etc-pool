bb7fc2ee13943143e3cbde79ff90545c01a91930
Marco Serra <marcoserra@pcbuster.it>
2018-10-30 10:56:02 +0100

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -5,8 +5,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/LeChuckDE/open-ethereumclassic-pool/rpc"
-	"github.com/LeChuckDE/open-ethereumclassic-pool/util"
+	"github.com/pcbuster/open-etc-pool/rpc"
+	"github.com/pcbuster/open-etc-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
