a7cf1986fa51cf6539140a543cbcb7754d9d554a
Marco Serra <marcoserra@pcbuster.it>
2018-10-30 09:30:19 +0100

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/LeChuckDE/open-ethereumclassic-pool/rpc"
-	"github.com/LeChuckDE/open-ethereumclassic-pool/storage"
+	"github.com/pcbuster/open-ethereumclassic-pool/rpc"
+	"github.com/pcbuster/open-ethereumclassic-pool/storage"
 )
 
 func TestMain(m *testing.M) {
