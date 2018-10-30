26626ae19fb893558a2904fbc7d051e7bb62506b
Marco Serra <marcoserra@pcbuster.it>
2018-10-30 10:49:48 +0100

    Update blocks.go

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -47,56 +47,6 @@ func (b Block) MixDigest() common.Hash   { return b.mixDigest }
 func (b Block) NumberU64() uint64        { return b.number }
 
 func (s *ProxyServer) fetchBlockTemplate() {
-	rpc := s.rpc()
-	t := s.currentBlockTemplate()
-	pendingReply, height, diff, err := s.fetchPendingBlock()
-	if err != nil {
-		log.Printf("Error while refreshing pending block on %s: %s", rpc.Name, err)
-		return
-	}
-	reply, err := rpc.GetWork()
-	if err != nil {
-		log.Printf("Error while refreshing block template on %s: %s", rpc.Name, err)
-		return
-	}
-	// No need to update, we have fresh job
-	if t != nil && t.Header == reply[0] {
-		return
-	}
-
-	pendingReply.Difficulty = util.ToHex(s.config.Proxy.Difficulty)
-
-	newTemplate := BlockTemplate{
-		Header:               reply[0],
-		Seed:                 reply[1],
-		Target:               reply[2],
-		Height:               height,
-		Difficulty:           big.NewInt(diff),
-		GetPendingBlockCache: pendingReply,
-		headers:              make(map[string]heightDiffPair),
-	}
-	// Copy job backlog and add current one
-	newTemplate.headers[reply[0]] = heightDiffPair{
-		diff:   util.TargetHexToDiff(reply[2]),
-		height: height,
-	}
-	if t != nil {
-		for k, v := range t.headers {
-			if v.height > height-maxBacklog {
-				newTemplate.headers[k] = v
-			}
-		}
-	}
-	s.blockTemplate.Store(&newTemplate)
-	log.Printf("New block to mine on %s at height %d / %s", rpc.Name, height, reply[0][0:10])
-
-	// Stratum
-	if s.config.Proxy.Stratum.Enabled {
-		go s.broadcastNewJobs()
-	}
-}
-
-func (s *ProxyServer) fetchBlockTemplate() {
 	r := s.rpc()
 	t := s.currentBlockTemplate()
 	reply, err := r.GetWork()
