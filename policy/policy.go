7a4a157a21c53adf2c49b71bc77898b0bc283b8b
Marco Serra <marcoserra@pcbuster.it>
2018-10-30 11:16:09 +0100

    Update policy.go

--- policy/policy.go
+++ policy/policy.go
@@ -9,8 +9,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/LeChuckDE/open-ethereumclassic-pool/storage"
-	"github.com/LeChuckDE/open-ethereumclassic-pool/util"
+	"github.com/pcbuster/open-etc-pool/storage"
+	"github.com/pcbuster/open-etc-pool/util"
 )
 
 type Config struct {
